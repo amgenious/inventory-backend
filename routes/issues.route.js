b3074b371593f44722fb9f4a36fbf7cb962d19eb
amgenious <henrytweneboah956@gmail.com>
2025-06-14 18:39:40 +0000

    done with receipt and issue

--- routes/issues.route.js
+++ routes/issues.route.js
@@ -1,6 +1,11 @@
 import {Router} from "express"
 const issueRouter = Router()
 import * as Controller from '../controllers/issue.controller.js'
+import multer from 'multer';
+import xlsx from 'xlsx';
+import fs from 'fs';
+
+const upload = multer({ dest: 'uploads/' });
 
 issueRouter.post('/add-issue', Controller.addIssues)
 issueRouter.get('/',Controller.getAllIssues)
@@ -8,5 +13,46 @@ issueRouter.get('/search', Controller.searchAllIssue)
 issueRouter.put('/update/:id', Controller.updateIssue)
 issueRouter.get('/search/report',Controller.searchIssue)
 
+issueRouter.post('/add-issuexlsx', upload.single('file'),async(req,res)=>{
+    const filePath = req.file?.path
+    if(!filePath){
+        return res.status(400).json({message: 'No file uploaded'})
+    }
+    try{
+        const workbook = xlsx.readFile(filePath);
+        const sheet = workbook.Sheets[workbook.SheetNames[0]];
+        const rows = xlsx.utils.sheet_to_json(sheet);  
+         
+        const sql = 'INSERT INTO issues(referencenumber,valuedate,transtype,transcode,customer,remarks,itemname,partnumber,location,quantity) VALUES (?,?,?,?,?,?,?,?,?,?)';
+        const insertStmt = DB.prepare(sql);
+
+         let remaining = rows.length;
+            if (remaining === 0) {
+              insertStmt.finalize();
+              fs.unlinkSync(filePath);
+              return res.status(400).json({ message: 'No data found in Excel file' });
+            }
+        for (const row of rows){
+            let {
+                referencenumber,
+                valuedate,transtype,transcode,customer,
+                remarks,itemname,partnumber,location,quantity
+            } = row;
+        if (!referencenumber || !itemname  || !location || !partnumber) {
+        remaining--;
+        if (remaining === 0) finalizeAndRespond();
+        continue;
+      }
+        }
+        function finalizeAndRespond() {
+              insertStmt.finalize();
+              fs.unlinkSync(filePath);
+              res.status(200).json({ message: 'Upload and import completed successfully' });
+    }
+    }catch(error){
+    console.error('Fatal error:', error);
+    res.status(500).json({ message: 'Error processing file', error: error.message });
+    }
+})
 
 export default issueRouter
--- routes/receipt.route.js
+++ routes/receipt.route.js
@@ -1,11 +1,58 @@
 import {Router} from "express"
 const receiptRouter = Router()
 import * as Controller from '../controllers/receipt.controller.js'
+import multer from 'multer';
+import xlsx from 'xlsx';
+import fs from 'fs';
+
+const upload = multer({ dest: 'uploads/' });
 
 receiptRouter.post('/add-receipt', Controller.addReceipt)
 receiptRouter.get('/', Controller.getAllReceipt)
 receiptRouter.get('/search', Controller.searchAllReceipt)
 receiptRouter.put('/update/:id', Controller.updateReceipt)
 receiptRouter.get('/search/report', Controller.searchReceipt)
+
+receiptRouter.post('/add-receiptxlsx', upload.single('file'),async(req,res)=>{
+    const filePath = req.file?.path
+    if(!filePath){
+        return res.status(400).json({message: 'No file uploaded'})
+    }
+    try{
+        const workbook = xlsx.readFile(filePath);
+        const sheet = workbook.Sheets[workbook.SheetNames[0]];
+        const rows = xlsx.utils.sheet_to_json(sheet);  
+         
+        const sql = 'INSERT INTO receipt(referencenumber,valuedate,transtype,transcode,invoicenumber,invoicedate,supplier,remarks,itemname,partnumber,location,quantity) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)';
+        const insertStmt = DB.prepare(sql);
+
+         let remaining = rows.length;
+            if (remaining === 0) {
+              insertStmt.finalize();
+              fs.unlinkSync(filePath);
+              return res.status(400).json({ message: 'No data found in Excel file' });
+            }
+        for (const row of rows){
+            let {
+                referencenumber,
+                valuedate,transtype,transcode,invoicenumber,invoicedate,supplier,
+                remarks,itemname,partnumber,location,quantity
+            } = row;
+        if (!referencenumber || !itemname  || !location || !partnumber) {
+        remaining--;
+        if (remaining === 0) finalizeAndRespond();
+        continue;
+      }
+        }
+        function finalizeAndRespond() {
+              insertStmt.finalize();
+              fs.unlinkSync(filePath);
+              res.status(200).json({ message: 'Upload and import completed successfully' });
+    }
+    }catch(error){
+    console.error('Fatal error:', error);
+    res.status(500).json({ message: 'Error processing file', error: error.message });
+    }
+})
 
 export default receiptRouter
